59b7bfe0cecc074319d9a3069ea2d693f3ebf329
Bisi3asi <kueeng8008@naver.com>
2023-11-06 15:21:07 +0900

    feat(controller) : return wise id(postWise)
    from repository

--- src/main/java/com/ll/WiseService.java
+++ src/main/java/com/ll/WiseService.java
@@ -3,7 +3,9 @@ package com.ll;
 public class WiseService {
     WiseRepository wiseRepository = new WiseRepository();
 
-    public void postWise(String content, String author){
-        wiseRepository.postWise(content, author);
+    // 생성할 명언을 repository로, 생성된 명언의 id를 controller로 전달
+    public int postWise(String content, String author){
+        int id = wiseRepository.postWise(content, author);
+        return id;
     }
 }
