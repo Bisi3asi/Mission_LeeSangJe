f5207cb325f83a2291a0e69cecf6a24a8685fcb3
Bisi3asi <kueeng8008@naver.com>
2023-11-06 17:39:23 +0900

    feat(repo) : add delete, find method
    * findWiseById : return index of wiseList
    * deleteWise : delete and return delete result

--- src/main/java/com/ll/WiseRepository.java
+++ src/main/java/com/ll/WiseRepository.java
@@ -9,10 +9,33 @@ public class WiseRepository {
     int postWise(String content, String author) {
         wiseList.add(new Wise(content, author));
         // 생성된 명언의 id값 리턴
-        return wiseList.get(wiseList.size()-1).getId();
+        return wiseList.get(wiseList.size() - 1).getId();
     }
 
-    List<Wise> getWiseList(){
+    List<Wise> getWiseList() {
         return wiseList;
+    }
+
+
+    /**
+     * @param id 찾고자 하는 명언의 id값
+     * @return 해당 id가 없을 시 -1 리턴 / 있을 시 해당 id 명언이 저장된 리스트 인덱스 리턴
+     */
+    public int findWisebyId(int id) {
+        for (int index = 0; index < wiseList.size(); index++){
+            if (wiseList.get(index).getId() == id) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public boolean deleteWise(int id) {
+        int deleteId = findWisebyId(id);
+        if (deleteId == -1) return false;
+        else {
+            wiseList.remove(deleteId);
+            return true;
+        }
     }
 }
