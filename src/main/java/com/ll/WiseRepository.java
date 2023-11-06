7be896fd9715fbc795b22ea8a28126747f45ac70
Bisi3asi <kueeng8008@naver.com>
2023-11-06 15:20:24 +0900

    feat(repo) : return wise id(postWise)
    from wiseList

--- src/main/java/com/ll/WiseRepository.java
+++ src/main/java/com/ll/WiseRepository.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class WiseRepository {
     List<Wise> wiseList = new ArrayList<>();
 
-    public void postWise(String content, String author) {
+    public int postWise(String content, String author) {
         wiseList.add(new Wise(content, author));
+        // 생성된 명언의 id값 리턴
+        return wiseList.get(wiseList.size()-1).getId();
     }
-
 }
