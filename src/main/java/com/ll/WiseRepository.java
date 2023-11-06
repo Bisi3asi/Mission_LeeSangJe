536164b388e929b72d296fdb2c3b9dcc6be9806f
Bisi3asi <kueeng8008@naver.com>
2023-11-06 16:33:46 +0900

    feat(repo) : add getWiseList method
    * getWiseList : returns wiseList

--- src/main/java/com/ll/WiseRepository.java
+++ src/main/java/com/ll/WiseRepository.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class WiseRepository {
     List<Wise> wiseList = new ArrayList<>();
 
-    public int postWise(String content, String author) {
+    int postWise(String content, String author) {
         wiseList.add(new Wise(content, author));
         // 생성된 명언의 id값 리턴
         return wiseList.get(wiseList.size()-1).getId();
+    }
+
+    List<Wise> getWiseList(){
+        return wiseList;
     }
 }
