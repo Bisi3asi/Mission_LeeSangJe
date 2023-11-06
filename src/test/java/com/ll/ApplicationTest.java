0e8904d2410151ce402c42dd4673f7e1b1ae9b2f
Bisi3asi <kueeng8008@naver.com>
2023-11-06 14:08:38 +0900

    test : level 2

--- src/test/java/com/ll/ApplicationTest.java
+++ src/test/java/com/ll/ApplicationTest.java
@@ -3,6 +3,8 @@ package com.ll;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,29 @@ public class ApplicationTest {
         String out = byteArrayOutputStream.toString();
 
         assertThat(input).isEqualTo("종료");
+        assertThat(out).isEqualTo("");
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+    }
+
+    @Test
+    @DisplayName("2단계 : 등록")
+    void t2() {
+        Scanner sc = TestUtil.genScanner("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """.stripIndent());
+        ArrayList<String> input = new ArrayList<>(){{
+            for (int i = 0; i < 4; i++){
+                add(sc.nextLine());
+            }
+        }};
+
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+        String out = byteArrayOutputStream.toString();
+
+        assertThat(input).isEqualTo(Arrays.asList("등록", "현재를 사랑하라.", "작자미상", "종료"));
         assertThat(out).isEqualTo("");
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
     }
