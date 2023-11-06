9bf726ce465739c152c504a835dced736a80e401
Bisi3asi <kueeng8008@naver.com>
2023-11-06 22:33:27 +0900

    feat & refactor(repo) : add modify method
    * refactor : itar to iter

--- src/main/java/com/ll/WiseRepository.java
+++ src/main/java/com/ll/WiseRepository.java
@@ -6,36 +6,40 @@ import java.util.List;
 public class WiseRepository {
     List<Wise> wiseList = new ArrayList<>();
 
-    int postWise(String content, String author) {
+    public int postWise(String content, String author) {
         wiseList.add(new Wise(content, author));
         // 생성된 명언의 id값 리턴
         return wiseList.get(wiseList.size() - 1).getId();
     }
 
-    List<Wise> getWiseList() {
+    public List<Wise> getWiseList() {
         return wiseList;
     }
-
 
     /**
      * @param id 찾고자 하는 명언의 id값
      * @return 해당 id가 없을 시 -1 리턴 / 있을 시 해당 id 명언이 저장된 리스트 인덱스 리턴
      */
-    public int findWisebyId(int id) {
-        for (int index = 0; index < wiseList.size(); index++){
-            if (wiseList.get(index).getId() == id) {
-                return index;
+    public int getWiseIndexById(int id) {
+        for (Wise wise : wiseList) {
+            if (wise.getId() == id) {
+                return wiseList.indexOf(wise);
             }
         }
         return -1;
     }
 
     public boolean deleteWise(int id) {
-        int deleteId = findWisebyId(id);
-        if (deleteId == -1) return false;
-        else {
-            wiseList.remove(deleteId);
+        int deleteIndex = getWiseIndexById(id);
+        if (deleteIndex == -1) {
+            return false;
+        } else {
+            wiseList.remove(deleteIndex);
             return true;
         }
+    }
+
+    public void modifyWise(int id, String content, String author){
+        wiseList.set(getWiseIndexById(id), new Wise(id, content, author));
     }
 }
