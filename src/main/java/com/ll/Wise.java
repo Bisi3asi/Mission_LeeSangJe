d836009828fc065e3202b2253c6be50afc978a9e
Bisi3asi <kueeng8008@naver.com>
2023-11-06 22:34:08 +0900

    feat(domain) : add Constructor
    * @RequiredArgsConstructor

--- src/main/java/com/ll/Wise.java
+++ src/main/java/com/ll/Wise.java
@@ -1,11 +1,13 @@
 package com.ll;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class Wise {
+    private static int nextId = 1;
     @Getter
     private final int id;
-    private static int nextId = 1;
     @Getter
     private final String content;
     @Getter
