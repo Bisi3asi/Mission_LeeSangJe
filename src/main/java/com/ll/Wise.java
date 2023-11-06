126fc173f2cd89b94e623fa08922d6d79aa483be
Bisi3asi <kueeng8008@naver.com>
2023-11-06 14:10:49 +0900

    feat(domain) : add class wise(level 2)

---
+++ src/main/java/com/ll/Wise.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Wise {
+    final private String content;
+    final private String author;
+}
